910e5f146a023f06fbd565b9b6e1a7c9b4478dd0
beginnerpark <gufdn10004@gmail.com>
2025-06-20 16:18:16 +0900

    Add Terraform EC2 deployment3

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "web" {
   instance_type               = "t2.micro"
   subnet_id                   = "subnet-0c337f838ab531e90"
   vpc_security_group_ids      = ["sg-083c47e5d04e47714"]
-  associate_public_ip_address = true
+  associate_public_ip_address = false
 
   tags = {
     Name = "existing-vpc-instance"
